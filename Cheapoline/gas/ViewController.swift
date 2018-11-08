56d59dd2d0d664c902c62d62903126a1f436a6f1
Kimber King <kimberharpuna@ucla.edu>
2018-11-08 15:42:25 -0800

    Added Splash Page UI

---
+++ Cheapoline/gas/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  gas
+//
+//  Created by Kimber King on 11/8/18.
+//  Copyright © 2018 Kimber King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func checkBoxTapped(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+            sender.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        }) { (success) in
+            sender.isSelected = !sender.isSelected
+            UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+                sender.transform = .identity
+            }, completion: nil)
+        }
+        /*if sender.isSelected {
+         sender.isSelected = false
+         } else {
+         sender.isSelected  = true
+         }*/
+    }
+
+
+}
+
