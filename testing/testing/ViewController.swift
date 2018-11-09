fa81d3a2db07954438c049a9e2274d194b5a8b0c
Brianna Tanusi <btanusi@gmail.com>
2018-11-08 16:42:11 -0800

    added more stations

--- testing/testing/ViewController.swift
+++ testing/testing/ViewController.swift
@@ -30,21 +30,39 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                         do {
                             let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: []) as! [String: AnyObject]
                             
-                            let stations = jsonResult["stations"] as! [Any]
-                            let a_station = stations[0] as! [String:Any]
+                            let all_stations = jsonResult["stations"] as! [Any]
                             
-                            let reg_price = a_station["reg_price"] as! AnyObject
-                            let mid_price = a_station["mid_price"] as! AnyObject
-                            let pre_price = a_station["pre_price"] as! AnyObject
-                            let diesel_price = a_station["diesel_price"] as! AnyObject
+                            //FOR STATION 1
+                            let station1 = all_stations[0] as! [String:Any]
+                            let station1_name = station1["station"] as! AnyObject
+                            let station1_distance = station1["distance"] as! AnyObject
+                            let station1_reg_price = station1["reg_price"] as! AnyObject
+                            let station1_mid_price = station1["mid_price"] as! AnyObject
+                            let station1_pre_price = station1["pre_price"] as! AnyObject
+                            let station1_diesel_price = station1["diesel_price"] as! AnyObject
                             
-                            print("hello")
-                            print(stations)
-                            print("prices")
-                            print(reg_price)
-                            print(mid_price)
-                            print(pre_price)
-                            print(diesel_price)
+                            //FOR STATION 2
+                            let station2 = all_stations[1] as! [String:Any]
+                            let station2_name = station2["station"] as AnyObject
+                            let station2_distance = station2["distance"] as AnyObject
+                            let station2_reg_price = station2["reg_price"] as AnyObject
+                            let station2_mid_price = station2["mid_price"] as AnyObject
+                            let station2_pre_price = station2["pre_price"] as AnyObject
+                            let station2_diesel_price = station2["diesel_price"] as AnyObject
+                            
+                            //FOR STATION 3
+                            let station3 = all_stations[2] as! [String:Any]
+                            let station3_name = station3["station"] as AnyObject
+                            let station3_distance = station3["distance"] as AnyObject
+                            let station3_reg_price = station3["reg_price"] as AnyObject
+                            let station3_mid_price = station3["mid_price"] as AnyObject
+                            let station3_pre_price = station3["pre_price"] as AnyObject
+                            let station3_diesel_price = station3["diesel_price"] as
+                            AnyObject
+                            
+                            print(station1_name)
+                            print(station1_distance)
+                            print(station1_reg_price)
                             
                         } catch {
                             print("Json Processing Failed")
