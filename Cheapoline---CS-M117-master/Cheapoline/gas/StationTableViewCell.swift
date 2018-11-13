1c8309ab1301c78c2fc8de3b6f84ae31fc76d104
elihan27 <ehan2016@gmail.com>
2018-11-13 14:10:00 -0800

    first commit

---
+++ Cheapoline---CS-M117-master/Cheapoline/gas/StationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StationTableViewCell.swift
+//  gas
+//
+//  Created by Elizabeth Han on 11/13/18.
+//  Copyright © 2018 Kimber King. All rights reserved.
+//
+
+import UIKit
+
+class StationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
