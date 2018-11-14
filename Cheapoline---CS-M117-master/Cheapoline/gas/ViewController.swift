6a2e7ffc72bdb4866273322bb44c4165188eb60e
Ian Costello <icostello@ucla.edu>
2018-11-13 16:37:30 -0800

    JSON Looping and Saving Station Info
    • I separated the find function into multiple functions, and implemented a completion handler so that we can access the station information in the "results" view we intend to implement.
    • Created the station struct to hold this information.
    • We now loop through the API requesting process 10 times (it is very unlikely that the API will fail this many times; if so, we can try to implement a notification to the user to try again).

--- Cheapoline---CS-M117-master/Cheapoline/gas/ViewController.swift
+++ Cheapoline---CS-M117-master/Cheapoline/gas/ViewController.swift
@@ -20,6 +20,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var milesInputVal = ""
     
+    struct station {
+        var name: String?
+        var distance: String?
+        var price: String?
+    }
+    
+    var station1 = station(name: "", distance: "", price: "")
+    var station2 = station(name: "", distance: "", price: "")
+    var station3 = station(name: "", distance: "", price: "")
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -107,12 +118,43 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     
+    
+    
+    
+    
+    
 
+    
     @IBAction func find(_ sender: Any) {
+        var count = 0
+        while (count < 10) {
+            find_helper(sender, withCompletion: {stations, error in
+                if error != nil {
+                    //handle error
+                } else if (stations != nil){
+                    self.parseJSON(stations!, Station1: &self.station1, Station2: &self.station2, Station3: &self.station3)
+                }
+            })
+            count += 1
+        }
+        
+        /*print(station1.name, station1.distance, station1.price)
+        print(station2.name, station2.distance, station2.price)
+        print(station3.name, station3.distance, station3.price)*/
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    func find_helper(_ sender: Any, withCompletion completion: @escaping ([Any]?, Error?) -> Void) {
+        
     //}
     //@IBAction func getLocation(_ sender: UIButton) {
         //"start" getting location
-        
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
             locationManager.startUpdatingLocation()
             currentLocation = locationManager.location
@@ -138,157 +180,152 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 buildURLString += "/reg/price/rfej9napna.json?"
             }
             
-            let urlString = URL(string: buildURLString)
             
+            let urlString = URL(string: buildURLString)
             if let url = urlString {
                 let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
                     if error != nil {
-                        print(error)
+                        completion(nil, error)
+                        return
                     } else {
                         if let urlContent = data {
                             do {
-                                let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: []) as! [String: AnyObject]
+                                guard let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: []) as? [String: AnyObject] else {completion(nil, nil);return}
                                 
-                                let all_stations = jsonResult["stations"] as! [Any]
-                                let num_of_stations = all_stations.count
-                                
-                                //IF THERE ARE NO STATIONS : NEED TO IMPLEMENT ERROR CHECKING
-                                if(num_of_stations <= 0){
-                                    print("no stations, please try again")
-                                }
-                                
-                                //IMPLEMENTATION FOR WHEN N/A OCCURS
-                                var countNAs = 0
-                                var i = 0
-                                while i < num_of_stations {
-                                    let tempStation = all_stations[i] as! [String:Any]
-                                    var price_check = ""
-                                    switch self.selectedType {
-                                    case .medium:
-                                        price_check = tempStation["mid_price"] as! String
-                                    case .premium:
-                                        price_check = tempStation["pre_price"] as! String
-                                    case .diesel:
-                                        price_check = tempStation["diesel_price"] as! String
-                                    default:
-                                        price_check = tempStation["reg_price"] as! String
-                                    }
-                                    if ((price_check) == "N/A") {
-                                        countNAs += 1
-                                    } else { break }
-                                    i += 1
-                                }
-                                i = 0
-                                var good_stations = all_stations
-                                while i < countNAs {
-                                    good_stations.remove(at: 0)
-                                    i += 1
-                                }
-                                
-                                var station1, station2, station3: [String:Any]
-                                var station1_name="", station2_name="", station3_name=""
-                                var station1_distance="", station2_distance="", station3_distance=""
-                                var station1_reg_price="", station2_reg_price="", station3_reg_price=""
-                                var station1_mid_price="", station2_mid_price="", station3_mid_price=""
-                                var station1_pre_price="", station2_pre_price="", station3_pre_price=""
-                                var station1_diesel_price="", station2_diesel_price="", station3_diesel_price=""
-                                
-                                if good_stations.count >= 1 {
-                                //FOR STATION 1
-                                station1 = good_stations[0] as! [String:Any]
-                                station1_name = station1["station"] as? String ?? "No Name Found"
-                                station1_distance = station1["distance"] as! String
-                                station1_reg_price = station1["reg_price"] as! String
-                                station1_mid_price = station1["mid_price"] as! String
-                                station1_pre_price = station1["pre_price"] as! String
-                                station1_diesel_price = station1["diesel_price"] as! String
-                                }
-                                
-                                if good_stations.count >= 2 {
-                                //FOR STATION 2
-                                station2 = good_stations[1] as! [String:Any]
-                                station2_name = station2["station"] as? String ?? "No Name Found"
-                                station2_distance = station2["distance"] as! String
-                                station2_reg_price = station2["reg_price"] as! String
-                                station2_mid_price = station2["mid_price"] as! String
-                                station2_pre_price = station2["pre_price"] as! String
-                                station2_diesel_price = station2["diesel_price"] as! String
-                                }
-                                
-                                if good_stations.count >= 3 {
-                                //FOR STATION 3
-                                station3 = good_stations[2] as! [String:Any]
-                                station3_name = station3["station"] as? String ?? "No Name Found"
-                                station3_distance = station3["distance"] as! String
-                                station3_reg_price = station3["reg_price"] as! String
-                                station3_mid_price = station3["mid_price"] as! String
-                                station3_pre_price = station3["pre_price"] as! String
-                                station3_diesel_price = station3["diesel_price"] as! String
-                                }
-                                
-                                print("jsonResult")
-                                print(jsonResult)
-                                print("all_stations")
-                                print(all_stations)
-                                print("number of stations")
-                                print(num_of_stations)
-                                if num_of_stations >= 1 {
-                                print("station1")
-                                print(station1_name)
-                                print(station1_distance)
-                                switch self.selectedType {
-                                case .medium:
-                                    print(station1_mid_price)
-                                case .premium:
-                                    print(station1_pre_price)
-                                case .diesel:
-                                    print(station1_diesel_price)
-                                default:
-                                    print(station1_reg_price)
-                                }
-                                }
-                                if num_of_stations >= 2 {
-                                print("station2")
-                                print(station2_name)
-                                print(station2_distance)
-                                switch self.selectedType {
-                                case .medium:
-                                    print(station2_mid_price)
-                                case .premium:
-                                    print(station2_pre_price)
-                                case .diesel:
-                                    print(station2_diesel_price)
-                                default:
-                                    print(station2_reg_price)
-                                }
-                                }
-                                if num_of_stations >= 3 {
-                                print("station3")
-                                print(station3_name)
-                                print(station3_distance)
-                                switch self.selectedType {
-                                case .medium:
-                                    print(station3_mid_price)
-                                case .premium:
-                                    print(station3_pre_price)
-                                case .diesel:
-                                    print(station3_diesel_price)
-                                default:
-                                    print(station3_reg_price)
-                                }
-                                }
+                                guard let all_stations = jsonResult["stations"] as? [Any] else {completion(nil, nil);return}
+                                completion(all_stations, nil)
                                 
                             } catch {
-                                print("Json Processing Failed")
+                                //print("Json Processing Failed")
                             }
                         }
                     }
                 }
                 task.resume()
             }
-            
         } else {
             locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func parseJSON(_ all_stations: [Any], Station1: inout station, Station2: inout station, Station3: inout station) {
+        let num_of_stations = all_stations.count
+        
+        //IF THERE ARE NO STATIONS : NEED TO IMPLEMENT ERROR CHECKING
+        if(num_of_stations <= 0){
+            print("no stations, please try again")
+        }
+        
+        //IMPLEMENTATION FOR WHEN N/A OCCURS
+        var countNAs = 0
+        var i = 0
+        while i < num_of_stations {
+            let tempStation = all_stations[i] as! [String:Any]
+            var price_check = ""
+            switch self.selectedType {
+            case .medium:
+                price_check = tempStation["mid_price"] as! String
+            case .premium:
+                price_check = tempStation["pre_price"] as! String
+            case .diesel:
+                price_check = tempStation["diesel_price"] as! String
+            default:
+                price_check = tempStation["reg_price"] as! String
+            }
+            if ((price_check) == "N/A") {
+                countNAs += 1
+            } else { break }
+            i += 1
+        }
+        i = 0
+        var good_stations = all_stations
+        while i < countNAs {
+            good_stations.remove(at: 0)
+            i += 1
+        }
+        
+        var station1, station2, station3: [String:Any]
+        var station1_name="", station2_name="", station3_name=""
+        var station1_distance="", station2_distance="", station3_distance=""
+        var station1_reg_price="", station2_reg_price="", station3_reg_price=""
+        var station1_mid_price="", station2_mid_price="", station3_mid_price=""
+        var station1_pre_price="", station2_pre_price="", station3_pre_price=""
+        var station1_diesel_price="", station2_diesel_price="", station3_diesel_price=""
+        
+        if good_stations.count >= 1 {
+            //FOR STATION 1
+            station1 = good_stations[0] as! [String:Any]
+            station1_name = station1["station"] as? String ?? "No Name Found"
+            station1_distance = station1["distance"] as! String
+            station1_reg_price = station1["reg_price"] as! String
+            station1_mid_price = station1["mid_price"] as! String
+            station1_pre_price = station1["pre_price"] as! String
+            station1_diesel_price = station1["diesel_price"] as! String
+        }
+        
+        if good_stations.count >= 2 {
+            //FOR STATION 2
+            station2 = good_stations[1] as! [String:Any]
+            station2_name = station2["station"] as? String ?? "No Name Found"
+            station2_distance = station2["distance"] as! String
+            station2_reg_price = station2["reg_price"] as! String
+            station2_mid_price = station2["mid_price"] as! String
+            station2_pre_price = station2["pre_price"] as! String
+            station2_diesel_price = station2["diesel_price"] as! String
+        }
+        
+        if good_stations.count >= 3 {
+            //FOR STATION 3
+            station3 = good_stations[2] as! [String:Any]
+            station3_name = station3["station"] as? String ?? "No Name Found"
+            station3_distance = station3["distance"] as! String
+            station3_reg_price = station3["reg_price"] as! String
+            station3_mid_price = station3["mid_price"] as! String
+            station3_pre_price = station3["pre_price"] as! String
+            station3_diesel_price = station3["diesel_price"] as! String
+        }
+        
+        if num_of_stations >= 1 {
+            Station1.name = station1_name
+            Station1.distance = station1_distance
+            switch self.selectedType {
+            case .medium:
+                Station1.price = station1_mid_price
+            case .premium:
+                Station1.price = station1_pre_price
+            case .diesel:
+                Station1.price = station1_diesel_price
+            default:
+                Station1.price = station1_reg_price
+            }
+        }
+        if num_of_stations >= 2 {
+            Station2.name = station2_name
+            Station2.distance = station2_distance
+            switch self.selectedType {
+            case .medium:
+                Station2.price = station2_mid_price
+            case .premium:
+                Station2.price = station2_pre_price
+            case .diesel:
+                Station2.price = station2_diesel_price
+            default:
+                Station2.price = station2_reg_price
+            }
+        }
+        if num_of_stations >= 3 {
+            Station3.name = station3_name
+            Station3.distance = station3_distance
+            switch self.selectedType {
+            case .medium:
+                Station3.price = station3_mid_price
+            case .premium:
+                Station3.price = station3_pre_price
+            case .diesel:
+                Station3.price = station3_diesel_price
+            default:
+                Station3.price = station3_reg_price
+            }
         }
     }
     
